c85759dd80bf7e333da94bf22d3b8468d2fe8632
Nguyen Ho Duy Tri <duytri.uit@gmail.com>
2018-01-07 18:09:06 +0700

    Fix Connect Remote Non-existence Document Database

--- src/main/scala/MainProgram.scala
+++ src/main/scala/MainProgram.scala
@@ -4,20 +4,20 @@ import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory
 
 object MainProgram {
   def main(args: Array[String]): Unit = {
-    val OrientDBUtils = new OrientDBUtils
+    /*val OrientDBUtils = new OrientDBUtils
     val factory: OrientGraphFactory = OrientDBUtils.connectDBUsingGraphAPI()
     val v1 = OrientDBUtils.insertVertex(factory, "Name", "Hoa")
     val v2 = OrientDBUtils.insertVertex(factory, "Name", "Tri")
     val edgeID = OrientDBUtils.insertEdge(factory, v1, v2)
     val datas = Map("Nguyen" -> "Name", "Ho" -> "Name", "Duy" -> "Name")
-    OrientDBUtils.insertVertexInBatches(factory, datas)
+    OrientDBUtils.insertVertexInBatches(factory, datas)*/
     
     val OrientDBUtilsDoc = new OrientDBUtils("remote:","localhost", "CoOccurrenceGraph","admin", "admin", "root", "12345")
     val pool = OrientDBUtilsDoc.connectDBUsingDocAPI
     OrientDBUtilsDoc.insertDoc(pool, "CCC", List(("Name", "DDDDDDDDD")))
     OrientDBUtilsDoc.insertDoc(pool, "DDD", List(("Name", "EEEEEEEEE")))
     
-    factory.close
+    //factory.close
     pool.close
   }
 }
